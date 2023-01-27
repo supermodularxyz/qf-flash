b9cfb9ec1e32ac7e6c1d4b6af87c699a530167b1
Carl Barrdahl <carlbarrdahl@gmail.com>
2023-01-27 14:51:03 +0100

    Mute Press to scan QR text slightly

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,7 +24,7 @@ const ScanButon = () => {
   return (
     <>
       {!hideInstructions ? (
-        <span className="absolute -left-48 top-6 flex items-center gap-4 pl-2">
+        <span className="absolute -left-48 top-6 flex items-center gap-4 pl-2 text-gray-500">
           Press to scan QR
           <ArrowRight className="h-4 w-4" />
         </span>
