3be6e88a2a9a2b93c8526792dc9b8631f3ef9572
Carl Barrdahl <carlbarrdahl@gmail.com>
2023-02-01 07:07:52 +0100

    Update BaseLayout

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -25,9 +25,9 @@ export const BaseLayout = ({ children }: PropsWithChildren) => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-gray-100 font-mono text-sm md:py-16">
+      <main className="h-screen bg-gray-100 font-mono text-sm md:py-16">
         <div
-          className="app container relative mx-auto h-full max-w-md bg-white md:rounded-xl md:shadow-2xl"
+          className="container relative mx-auto h-full max-w-md bg-white md:rounded-xl md:shadow-2xl"
           style={{ maxHeight: 851, height: `calc(var(--vh, 1vh) * 100)` }}
         >
           {children}
