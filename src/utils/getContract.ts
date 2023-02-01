2a30ca5bfebdbbc63a3ed3d50ca04c09a013043a
Carl Barrdahl <carlbarrdahl@gmail.com>
2023-02-01 07:02:59 +0100

    Update contract

--- src/utils/getContract.ts
+++ src/utils/getContract.ts
@@ -1,7 +1,7 @@
 import type { Wallet } from "ethers";
-import { Erc777__factory } from "types";
+import { QFToken__factory } from "types";
 
 const tokenAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as string;
 
 export const getContract = (signer: Wallet | null) =>
-  Erc777__factory.connect(tokenAddress, signer as Wallet);
+  QFToken__factory.connect(tokenAddress, signer as Wallet);
