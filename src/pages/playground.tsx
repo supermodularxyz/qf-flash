e37f1498d31e04f0ce6998f1b941c14bacd2aa88
Carl Barrdahl <carlbarrdahl@gmail.com>
2023-02-01 09:42:35 +0100

    Remove console.log

--- src/pages/playground.tsx
+++ src/pages/playground.tsx
@@ -53,7 +53,6 @@ const Playground: NextPage = () => {
               </h4>
               <div className="grid grid-cols-2 gap-4">
                 {Object.entries(group).map(([address, mnemonic]) => {
-                  console.log(mnemonic);
                   return (
                     <Link
                       key={address}
