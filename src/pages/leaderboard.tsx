af3e18fe49267951a9c26a2a9a881eabbaca12ea
Carl Barrdahl <carlbarrdahl@gmail.com>
2023-02-04 08:00:39 +0100

    Improve UI in leaderboard

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -7,6 +7,8 @@ import { timeAgo } from "utils/date";
 import { Skeleton } from "components/Skeleton";
 import { useName } from "hooks/useName";
 import { ScanButton } from "components/ScanButton";
+import { roleEmoji } from "utils/roles";
+import { ComponentPropsWithoutRef, ReactNode } from "react";
 
 const createLoadingProjects = (length = 2) =>
   Array.from({ length })
@@ -27,30 +29,43 @@ const Name = ({ children = "" }) => {
   const name = useName(children);
   return (
     <Skeleton className="w-full" isLoading={name.isLoading}>
-      {name.data || truncate(children)}
+      {name.data || truncate(children, 8)}
     </Skeleton>
   );
 };
+
+const LeaderboardTable = ({
+  header,
+  ...props
+}: { header: ReactNode } & ComponentPropsWithoutRef<"table">) => (
+  <>
+    <div className="mb-1 text-xs uppercase tracking-widest text-gray-600">
+      {header}
+    </div>
+
+    <div className="mb-4 max-h-60 overflow-y-scroll">
+      <table className="w-full table-auto text-sm" {...props} />
+    </div>
+  </>
+);
+
 const Leaderboard: NextPage = () => {
   const { data, isLoading } = useLeaderboard();
 
   const { bees = [], projects = [], queryDuration, lastUpdated } = data || {};
 
   const loadingProjects = createLoadingProjects(3);
-  console.log(projects);
-  console.log(bees);
   return (
     <Layout fab={<ScanButton />}>
       <div className="mb-4 text-sm uppercase tracking-widest">Leaderboard</div>
 
-      <table className="mb-4 w-full table-auto text-sm">
+      <LeaderboardTable header={<>{roleEmoji[2]} Flowers</>}>
         <thead>
           <tr className="text-left text-xs">
-            <th>Tokens</th>
-            <th>Flower</th>
+            <th>Received</th>
+            <th>Address</th>
             <th className="text-right">Visits</th>
             <th className="text-right">Matching</th>
-            {/* <th className="text-right">Last funded</th> */}
           </tr>
         </thead>
         <tbody>
@@ -58,7 +73,7 @@ const Leaderboard: NextPage = () => {
             ({ address = "", amount = 0, funders = [], matching = 0 }) => {
               return (
                 <tr key={address}>
-                  <td className="w-16">
+                  <td className="w-20">
                     <Skeleton className="w-12" isLoading={isLoading}>
                       {amount}
                     </Skeleton>
@@ -84,50 +99,38 @@ const Leaderboard: NextPage = () => {
             }
           )}
         </tbody>
-      </table>
-      <table className="mb-4 w-full table-auto text-sm">
+      </LeaderboardTable>
+      <LeaderboardTable header={<>{roleEmoji[1]} Bees</>}>
         <thead>
           <tr className="text-left text-xs">
-            <th>Tokens</th>
-            <th>Bee</th>
-            <th className="text-right">Visited</th>
-            <th className="text-right"></th>
-            {/* <th className="text-right">Last funded</th> */}
+            <th>Sent</th>
+            <th>Address</th>
+            <th className="text-right">Flowers visited</th>
           </tr>
         </thead>
         <tbody>
-          {bees.map(({ address = "", amount = 0, visited = 0 }) => {
-            return (
-              <tr key={address}>
-                <td className="w-16">
-                  <Skeleton className="w-12" isLoading={isLoading}>
-                    {amount}
-                  </Skeleton>
-                </td>
-                <td>
-                  <Name>{address}</Name>
-                </td>
-                <td className="w-20 text-right">
-                  <Skeleton className="w-12" isLoading={isLoading}>
-                    {visited}
-                  </Skeleton>
-                </td>
-                <td className="w-28 text-right">
-                  <Skeleton className="w-12" isLoading={isLoading}>
-                    {/* {(matching || 0).toFixed(2)} DAI */}
-                    {/* {timeAgo(lastFunded)} ago */}
-                  </Skeleton>
-                </td>
-              </tr>
-            );
-          })}
+          {bees.map(({ address = "", amount = 0, visited = 0 }) => (
+            <tr key={address}>
+              <td className="w-16">
+                <Skeleton className="w-12" isLoading={isLoading}>
+                  {amount}
+                </Skeleton>
+              </td>
+              <td>
+                <Name>{address}</Name>
+              </td>
+              <td className=" text-right">
+                <Skeleton className="w-12" isLoading={isLoading}>
+                  {visited}
+                </Skeleton>
+              </td>
+            </tr>
+          ))}
         </tbody>
-      </table>
-
+      </LeaderboardTable>
       <div className="flex justify-between text-xs">
         <div>
-          Last updated:{" "}
-          {lastUpdated && <span>&lt; {timeAgo(lastUpdated)}</span>}
+          Last updated: {lastUpdated && <span>&lt;{timeAgo(lastUpdated)}</span>}
         </div>
         <div>Query time: {queryDuration}ms</div>
       </div>
